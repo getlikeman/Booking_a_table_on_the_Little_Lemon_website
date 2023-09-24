6c8fedb755b12f7b09fabc31ce7c84eb95a11046
getlikeman <misternikir@gmail.com>
2023-09-24 17:33:28 +0600

    delete

--- src/Components/HeroImage.jsx
+++ src/Components/HeroImage.jsx
@@ -1,5 +1,5 @@
 export default function HeroImage() {
     return (<div className={'w-100 container-fluid heroImage d-flex align-items-center'}>
-        <img src="/src/assets/Asset%2016@4x.png" alt="" className={'mx-2 img-fluid'} width={812}/>
+        <img src="./Asset%2016@4x.png" alt="" className={'mx-2 img-fluid'} width={812}/>
     </div>)
 }
